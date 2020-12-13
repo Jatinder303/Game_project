379e4e62ca51be360118df177bfd6e6042b57fae
Jatinder Singh <Jatinder303@gmail.com>
2020-12-14 10:09:52 +1300

    Unit Testing

--- Game_UnitTestProject/UnitTest1.cs
+++ Game_UnitTestProject/UnitTest1.cs
@@ -1,14 +1,33 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using Game_project;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Game_UnitTestProject
 {
     [TestClass]
     public class UnitTest1
     {
+        Game_Logic obj_GameLogic = new Game_Logic();
         [TestMethod]
-        public void TestMethod1()
+        public void Fire()
         {
+            obj_GameLogic.Hammer_Position = 1;
+            obj_GameLogic.Bullet_Position = 1;
+
+            int ExpectedResult = obj_GameLogic.shoot();
+            Assert.AreEqual(1, ExpectedResult);
+
+
+        }
+        [TestMethod]
+        public void NotFire()
+        {
+            obj_GameLogic.Hammer_Position = 1;
+            obj_GameLogic.Bullet_Position = 5;
+
+            int ExpectedResult = obj_GameLogic.shoot();
+            Assert.AreEqual(0, ExpectedResult);
+
+
         }
     }
 }
